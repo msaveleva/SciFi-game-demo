af7c4fb41882e5eda82bcb9e4f5b455b7b2aec8a
Maria Saveleva <mariiasaveleva@gmail.com>
2018-05-13 15:04:08 +0300

    Added ammo system

--- Sci-Fi Demo/Assets/Game/Scripts/Player.cs
+++ Sci-Fi Demo/Assets/Game/Scripts/Player.cs
@@ -8,19 +8,31 @@ public class Player : MonoBehaviour {
 
 
 	[SerializeField]
-	private float speed = 3.5f;
+	private float _speed = 3.5f;
+    [SerializeField]
+    private int _currentAmo;
+    [SerializeField]
+    private float _reloadTimeout = 1.5f;
     [SerializeField]
     private GameObject _muzzleFlash;
     [SerializeField]
     private GameObject _hitMarker;
+    [SerializeField]
+    private AudioSource _weapoAudio;
 
 	private CharacterController _charaController;
+    private UIManager _uiManager;
+
+    private int _maxAmo = 50;
+    private bool _isReloading = false;
 
 	// Use this for initialization
 	void Start () {
 		_charaController = GetComponent<CharacterController> ();
 
         Cursor.lockState = CursorLockMode.Locked;
+        _currentAmo = _maxAmo;
+        _uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
 	}
 	
 	// Update is called once per frame
@@ -30,32 +42,61 @@ public class Player : MonoBehaviour {
             Cursor.lockState = CursorLockMode.None;
         }
 
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetKey(KeyCode.Mouse0) && _currentAmo > 0)
         {
-            _muzzleFlash.SetActive(true);
-
-            Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit raycastHitInfo;
-            if (Physics.Raycast(rayOrigin, out raycastHitInfo))
-            {
-                Debug.Log("Hit: " + raycastHitInfo.transform.name);
-                GameObject hitMarker = Instantiate(_hitMarker, raycastHitInfo.point, Quaternion.LookRotation(raycastHitInfo.normal)) as GameObject;
-                Destroy(hitMarker, 0.2f);
-            }
+            _Shoot();
         }
         else
         {
+            _weapoAudio.Stop();
             _muzzleFlash.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R) && _isReloading == false)
+        {
+            _isReloading = true;
+            StartCoroutine(_Reload());
         }
 
         _MovePlayer();
 	}
 
+    void _Shoot()
+    {
+        _currentAmo--;
+        _uiManager.UpdateAmoTitle(_currentAmo);
+
+        if (!_weapoAudio.isPlaying)
+        {
+            _weapoAudio.Play();
+        }
+
+        _muzzleFlash.SetActive(true);
+
+        Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit raycastHitInfo;
+        if (Physics.Raycast(rayOrigin, out raycastHitInfo))
+        {
+            //Debug.Log("Hit: " + raycastHitInfo.transform.name);
+            GameObject hitMarker = Instantiate(_hitMarker, raycastHitInfo.point, Quaternion.LookRotation(raycastHitInfo.normal)) as GameObject;
+            Destroy(hitMarker, 0.2f);
+        }
+    }
+
+    IEnumerator _Reload()
+    {
+        yield return new WaitForSeconds(_reloadTimeout);
+        _currentAmo = _maxAmo;
+        _uiManager.UpdateAmoTitle(_currentAmo);
+        _isReloading = false;
+        Debug.Log("Reloaded");
+    }
+
     void _MovePlayer() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
-        Vector3 velocity = direction * speed;
+        Vector3 velocity = direction * _speed;
         velocity.y -= kGravity;
 
         //transform to global coordinates.
---
+++ Sci-Fi Demo/Assets/Game/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField]
+    private Text _ammoText;
+
+	public void UpdateAmoTitle(int ammoCount)
+    {
+        _ammoText.text = "Ammo: " + ammoCount;
+    }
+}
